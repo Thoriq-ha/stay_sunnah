b94ac2dfb144fd1a058b4cd484810f3747fd87be
Thoriq-ha <ithoriq0@gmail.com>
2022-04-08 04:28:35 +0700

    crud

--- lib/app/data/local/db_helper.dart
+++ lib/app/data/local/db_helper.dart
@@ -1,37 +1,61 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:stay_sunnah/app/data/local/task.dart';
+import 'package:stay_sunnah/app/data/local/task_schema.dart';
 
 class DBHelper {
-  static Database? _db;
-  static final int _version = 1;
-  static final String _tableName = "tasks";
+  static const int _version = 1;
+  static const String _tableName = "tasks";
 
-  static Future<void> initDb() async {
-    if (_db != null) {
-      return;
-    }
-    try {
-      String _path = await getDatabasesPath() + 'tasks.db';
-      _db =
-          await openDatabase(_path, version: _version, onCreate: (db, version) {
-        print("creating a new one");
-        return db.execute(
-          "CREATE TABLE $_tableName("
-          "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-          "title STRING, note TEXT, date STRING, "
-          "startTime STRING, endTime STRING, "
-          "remind INTEGER, repeat STRING, "
-          "color INTEGER, "
-          "isCompleted INTEGER)",
-        );
-      });
-    } catch (e) {
-      print(e);
-    }
+  static Future<void> createTables(Database database) async {
+    await database.execute("""CREATE TABLE tasks(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        title TEXT,
+        note TEXT,
+        isCompleted INTEGER,
+        date TEXT,
+        startTime TEXT,
+        endTime TEXT,
+        color INTEGER,
+        remind INTEGER,
+        repeat INTEGER
+      )
+      """);
   }
 
-  static Future<int> insert(Task? task) async {
-    print("insert function called");
-    return await _db?.insert(_tableName, task!.toJson()) ?? 1;
+  static Future<Database> db() async {
+    return openDatabase(
+      'tasks.db',
+      version: _version,
+      onCreate: (Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  static Future<int> createItem(Task data) async {
+    print("Insert Item function called");
+    final db = await DBHelper.db();
+    final id = await db.insert(_tableName, data.toJson(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+    return id;
+  }
+
+  static Future<List<Map<String, dynamic>>> getItems() async {
+    print("Get Item function called");
+    final db = await DBHelper.db();
+    return db.query(_tableName, orderBy: "id");
+  }
+
+  // Delete
+  static Future<void> deleteItem(int id) async {
+    print("Delete function called");
+    final db = await DBHelper.db();
+    try {
+      await db.delete(_tableName, where: "id = ?", whereArgs: [id]);
+    } catch (err) {
+      debugPrint("Something went wrong when deleting an item: $err");
+    }
   }
 }
--- lib/app/modules/sunnah/controllers/sunnah_controller.dart
+++ lib/app/modules/sunnah/controllers/sunnah_controller.dart
@@ -1,5 +1,58 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:stay_sunnah/app/data/local/db_helper.dart';
 
-class SunnahController extends GetxController {
-  var selectedDate = DateTime.now().obs;
+import '../../../data/local/task_schema.dart';
+
+class SunnahController extends GetxController
+    with StateMixin<List<Map<String, dynamic>>> {
+  // var selectedDate = DateTime.now().obs;
+  List<Map<String, dynamic>> listTask = [];
+
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController noteController = TextEditingController();
+  final Rx<DateTime> selectedDate = DateTime.now().obs;
+
+  final RxString selectedRepeat = "None".obs;
+  final RxInt selectedColor = 0.obs;
+
+  Future<int> addTask({Task? task}) async {
+    return await DBHelper.createItem(task!);
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    refreshSunnah();
+  }
+
+  @override
+  void onClose() {
+    print("Onclose");
+    super.onClose();
+  }
+
+  @override
+  // TODO: implement onStart
+  InternalFinalCallback<void> get onStart {
+    print("OnStart");
+    return super.onStart;
+  }
+
+  @override
+  void onReady() {
+    print('On Ready');
+    super.onReady();
+  }
+
+  void refreshSunnah() async {
+    final data = await DBHelper.getItems();
+    listTask = data;
+    change(listTask, status: RxStatus.success());
+  }
+
+  void deleteItem(int id) async {
+    await DBHelper.deleteItem(id);
+    refreshSunnah();
+  }
 }
